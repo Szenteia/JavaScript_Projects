a1c3661662355d88092fdb85ec5b8a84fbd33862
Szentei András <szenteia@gmail.com>
2024-06-16 17:31:12 +0200

    leftArrow_visible

--- fitness/lib/pages/home.dart
+++ fitness/lib/pages/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -19,7 +21,19 @@ class HomePage extends StatelessWidget {
       backgroundColor: Colors.white,
       elevation: 0.0,
       centerTitle: true,
+      leading: Container(
+        margin: EdgeInsets.all(10),
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+        color: Color(0xffF7F8F8),
+        borderRadius:BorderRadius.circular(10),
+      ),
+        child: SvgPicture.asset('assets/icons/left.svg',
+        height: 20,
+        width: 20,
+        ),
     ),
+    )
     );
   }
 }
