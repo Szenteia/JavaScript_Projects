bf08d7bab1e4ff42ce7de72ddf847e6b1680f383
Szentei András <szenteia@gmail.com>
2024-06-16 12:31:28 +0200

    Sorting_InitFlutter

--- fiveMiniProject/two/two/src/App.js
+++ fiveMiniProject/two/two/src/App.js
@@ -1,10 +1,14 @@
 import DigitalClock from './DigitalClock';
+import Accordian from '.src/components/accordian/index.jsx';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+
       <DigitalClock />
+      <Accordian />
     </div>
   );
 }
