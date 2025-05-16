cfc3cc721fb6c7e2d9be79f7c2285a9f02c5fb05
Szentei András <szenteia@gmail.com>
2025-05-16 20:47:28 +0200

    appJsx_init

--- PF/src/main.jsx
+++ PF/src/main.jsx
@@ -1,10 +1,4 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import reactDOM from "react-dom/client";
+import App from "./App";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+reactDOM.createRoot(document.getElementById('root')).render(<App />);
