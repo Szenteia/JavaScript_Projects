88a1e1af3c51b8ea17cdeeddacb5c56e4ca35ca7
Szentei András <szenteia@gmail.com>
2025-05-22 10:39:54 +0200

    Added_drei_toAnimate_html

--- PF/src/components/Loader.jsx
+++ PF/src/components/Loader.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import { Html } from '@react-three/drei'
 
 const Loader = () => {
   return (
-    <div>Loader</div>
+    <Html>
+    <div className='flex justify-center items-center'>
+        <div className=' w-20 h-20 border-2 border-opacity-20 border-black-500 border-t-black-500 rounded-full animate-spin'/>
+    </div>
+    </Html>
   )
 }
 
