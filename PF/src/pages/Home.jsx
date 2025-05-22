ce13ec85a3d1a59e991b87ae2f57ea1fbf768bd3
Szentei András <szenteia@gmail.com>
2025-05-22 10:28:34 +0200

    adding_Three/fiber

--- PF/src/pages/Home.jsx
+++ PF/src/pages/Home.jsx
@@ -6,6 +6,9 @@ const Home = () => {
       <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
         POPUP
       </div>
+      <Canvas>
+        
+      </Canvas>
 
     </section>
   )
