a9ecb1366116642e8f1bb80170853cebcc2a22e5
Szentei András <szenteia@gmail.com>
2025-05-22 10:35:15 +0200

    LoadingScreen_added

--- PF/src/pages/Home.jsx
+++ PF/src/pages/Home.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import { Suspense } from 'react'
+import { Canvas } from '@react-three/fiber'
+import Loader from '../components/Loader.jsx'
 
 const Home = () => {
   return (
@@ -6,8 +8,13 @@ const Home = () => {
       <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
         POPUP
       </div>
-      <Canvas>
-        
+      <Canvas className='
+      w-full h-screen bg-transparent'
+      camera={{near: 0.1, far: 1000}}
+      >
+        <Suspense fallback={<Loader/>}>
+
+        </Suspense>
       </Canvas>
 
     </section>
