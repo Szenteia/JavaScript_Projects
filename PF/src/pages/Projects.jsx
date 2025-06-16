0928e9b1f49c1f204ddb6d63272e3aa396229dfa
Szentei András <szenteia@gmail.com>
2025-06-16 18:00:11 +0200

    Projects_init

--- PF/src/pages/Projects.jsx
+++ PF/src/pages/Projects.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <section className='max-container'>
+      <h1 className='head-text'>Hello, I'm <span className='blue-gradient_text font-semibold drop-shadow'>András</span>
+      </h1>
+
+      <div className='mt-5 flex flex-col gap-3 text-slate-500'>
+        <p>Software Engineer based in Hungary, I have a passion for gamification with programming, 3D visualization  and application programming.</p>
+      </div>
+      </section>
   )
 }
 
