152329c942362ce9501e6cf909cb5e2be6238416
Szentei András <szenteia@gmail.com>
2025-06-16 17:27:15 +0200

    About_init

---
+++ PF/src/constants/index.js
@@ -0,0 +1,208 @@
+import { meta, shopify, starbucks, tesla } from "../assets/images";
+import {
+    contact,
+    css,
+    estate,
+    express,
+    git,
+    github,
+    html,
+    javascript,
+    linkedin,
+    mongodb,
+    motion,
+    nextjs,
+    nodejs,
+    pricewise,
+    react,
+    redux,
+    snapgram,
+    summiz,
+    tailwindcss,
+    threads,
+    typescript
+} from "../assets/icons";
+
+export const skills = [
+    {
+        imageUrl: css,
+        name: "CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: express,
+        name: "Express",
+        type: "Backend",
+    },
+    {
+        imageUrl: git,
+        name: "Git",
+        type: "Version Control",
+    },
+    {
+        imageUrl: github,
+        name: "GitHub",
+        type: "Version Control",
+    },
+    {
+        imageUrl: html,
+        name: "HTML",
+        type: "Frontend",
+    },
+    {
+        imageUrl: javascript,
+        name: "JavaScript",
+        type: "Frontend",
+    },
+    {
+        imageUrl: mongodb,
+        name: "MongoDB",
+        type: "Database",
+    },
+    {
+        imageUrl: motion,
+        name: "Motion",
+        type: "Animation",
+    },
+    {
+        imageUrl: nextjs,
+        name: "Next.js",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nodejs,
+        name: "Node.js",
+        type: "Backend",
+    },
+    {
+        imageUrl: react,
+        name: "React",
+        type: "Frontend",
+    },
+    {
+        imageUrl: redux,
+        name: "Redux",
+        type: "State Management",
+    },
+    {
+        imageUrl: tailwindcss,
+        name: "Tailwind CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: typescript,
+        name: "TypeScript",
+        type: "Frontend",
+    }
+];
+
+export const experiences = [
+    {
+        title: "React.js Developer",
+        company_name: "Starbucks",
+        icon: starbucks,
+        iconBg: "#accbe1",
+        date: "March 2020 - April 2021",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+    {
+        title: "React Native Developer",
+        company_name: "Tesla",
+        icon: tesla,
+        iconBg: "#fbc3bc",
+        date: "Jan 2021 - Feb 2022",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+    {
+        title: "Web Developer",
+        company_name: "Shopify",
+        icon: shopify,
+        iconBg: "#b7e4c7",
+        date: "Jan 2022 - Jan 2023",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+    {
+        title: "Full stack Developer",
+        company_name: "Meta",
+        icon: meta,
+        iconBg: "#a2d2ff",
+        date: "Jan 2023 - Present",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+];
+
+export const socialLinks = [
+    {
+        name: 'Contact',
+        iconUrl: contact,
+        link: '/contact',
+    },
+    {
+        name: 'GitHub',
+        iconUrl: github,
+        link: 'https://github.com/Szenteia',
+    },
+    {
+        name: 'LinkedIn',
+        iconUrl: linkedin,
+        link: 'https://www.linkedin.com/in/andrás-szentei-37849b1bb',
+    }
+];
+
+export const projects = [
+    {
+        iconUrl: threads,
+        theme: 'btn-back-green',
+        name: 'Imagination of a working Neural system with CSS',
+        description: 'Created a small project using CSS to generate connections like i imagine the neurat growth.',
+        link: 'https://szenteia.github.io/JavaScript_Projects/neuralSys/index.html',
+    },
+    {
+        iconUrl: summiz,
+        theme: 'btn-back-blue',
+        name: 'Retro Design Switcher',
+        description: 'A webapplication where you can change styles by decades style and you get information about the decades style in general with some capturing images.',
+        link: 'https://szenteia.github.io/JavaScript_Projects/RetroDesign/index.html',
+    },
+    {
+        iconUrl: snapgram,
+        theme: 'btn-back-pink',
+        name: 'Anime Style AnimeList',
+        description: 'Built a webapp where you can check the lasest animes running, choose and store your favourites up to three.',
+        link: 'https://szenteia.github.io/JavaScript_Projects/animeStyle/index.html',
+    },
+        {
+        iconUrl: pricewise,
+        theme: 'btn-back-red',
+        name: 'Project Sushi',
+        description: 'Developed a web application frontend that can be used ad a Sushi ordering landing page.',
+        link: 'https://szenteia.github.io/JavaScript_Projects/HtmlCss/index.html',
+    },
+    {
+        iconUrl: estate,
+        theme: 'btn-back-black',
+        name: 'Rock Paper Scissor Lizard Spock',
+        description: 'Developed a webapp minigame, where you play the classic rock-paper-scissor with a little spiced up rules.',
+        link: 'https://szenteia.github.io/JavaScript_Projects/fiveMiniProject/RockPaperScissor/index.html',
+    },
+];
--- PF/src/pages/About.jsx
+++ PF/src/pages/About.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <section className='max-container'>
+      <h1 className='head-text'>Hello, I'm <span className='blue-gradient_text font-semibold drop-shadow'>András</span>
+      </h1>
+
+      <div className='mt-5 flex flex-col gap-3 text-slate-500'>
+        <p>Software Engineer based in Hungary, I have a passion for gamification with programming, 3D visualization  and application programming.</p>
+      </div>
+
+      <div className='py-10 flex flex-col'>
+      <h3 className='subhead-text'>My Skills</h3>
+        <div className='mt-16 flex flex-wrap gap-12'>
+
+        </div>
+      </div>
+      </section>
   )
 }
 
