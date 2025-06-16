b7d062ed338ce322067c1d91b535e1e701abe9eb
Szentei András <szenteia@gmail.com>
2025-06-16 17:58:21 +0200

    Rotation_fixed_felt_pixelBypixel_nowFine

--- PF/src/components/CTA.jsx
+++ PF/src/components/CTA.jsx
@@ -7,7 +7,8 @@ const CTA = () => {
         <p className='cta-text'> Have a project in mind? <br className='sm:block hidden' /> 
         Let's build something together!</p>
         <Link to='/contact' className='btn'>
-        Contact</Link>
+        Contact
+        </Link>
     </section>
   )
 }
