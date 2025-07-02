fabfe73ee58580b17907e7005ec17d920edede8c
Szentei András <szenteia@gmail.com>
2025-07-02 18:31:05 +0200

    button_dinamic_type_title_icon_variant

--- travel_app/components/Navbar.tsx
+++ travel_app/components/Navbar.tsx
@@ -2,20 +2,30 @@ import Link from 'next/link'
 import React from 'react'
 import Image from 'next/image'
 import { NAV_LINKS } from '@/constants'
+import Button from './Button'
 const Navbar = () => {
   return (
     <nav className='flexBetween max-container padding-container relative z-20 py-2'>
         <Link
         href="/">
           <Image src='/hilink-logo.svg' alt='logo' width={80} height={29} />
-          <ul className='hidden h-full gap-12 lg:flex'>
+
+        </Link>
+        <ul className='hidden h-full gap-12 lg:flex'>
             {NAV_LINKS.map((link)=>(
               <Link
-              href={link.href} key={link.key}> {link.label}
+              href={link.href} key={link.key} className="regular-16 text-grey-50 felxCenter cursor-pointer pb-1.5 transition-all hover:font-bold">  {link.label}
               </Link>
             ))}
-          </ul>
-        </Link>
+        </ul>
+        <div className='lg:flexCenter hidden'>
+            <Button
+            type='button'
+            title='Login'
+            icon='/user.svg'
+            variant='btn_dark_green'
+            />
+        </div>
         </nav>
   )
 }
