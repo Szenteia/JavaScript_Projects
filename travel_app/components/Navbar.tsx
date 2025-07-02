2c816748ac1412943a1089299b427db9d2dc0256
Szentei András <szenteia@gmail.com>
2025-07-02 18:11:04 +0200

    nav Links added

--- travel_app/components/Navbar.tsx
+++ travel_app/components/Navbar.tsx
@@ -1,12 +1,20 @@
 import Link from 'next/link'
 import React from 'react'
-
+import Image from 'next/image'
+import { NAV_LINKS } from '@/constants'
 const Navbar = () => {
   return (
-    <nav className='border-2 border-red-400 flexBetween max-container padding-container relative z-30 py-5'>
+    <nav className='flexBetween max-container padding-container relative z-20 py-2'>
         <Link
         href="/">
-            
+          <Image src='/hilink-logo.svg' alt='logo' width={80} height={29} />
+          <ul className='hidden h-full gap-12 lg:flex'>
+            {NAV_LINKS.map((link)=>(
+              <Link
+              href={link.href} key={link.key}> {link.label}
+              </Link>
+            ))}
+          </ul>
         </Link>
         </nav>
   )
