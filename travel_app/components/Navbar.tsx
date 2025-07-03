c0433584e3f82b25ddc53cf0d11bb41986d2ba89
Szentei András <szenteia@gmail.com>
2025-07-03 08:07:30 +0200

    added_gsap_aminated_logo_repeat5secondSpin

--- travel_app/components/Navbar.tsx
+++ travel_app/components/Navbar.tsx
@@ -1,32 +1,77 @@
+'use client'
+
+import React, { useEffect, useRef } from 'react'
 import Link from 'next/link'
-import React from 'react'
 import Image from 'next/image'
 import { NAV_LINKS } from '@/constants'
 import Button from './Button'
+import gsap from 'gsap'
+
 const Navbar = () => {
+  const logoRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    const tl = gsap.timeline()
+
+    tl.fromTo(
+      logoRef.current,
+      { x: '-100%', opacity: 0 },
+      {
+        x: '0%',
+        opacity: 1,
+        duration: 1.2,
+        ease: 'power3.out',
+      }
+    )
+    .to(logoRef.current, {
+      rotate: 720,
+      duration: 1.5,
+      ease: 'power2.out',
+    })
+const interval = setInterval(() => {
+    gsap.fromTo(
+      logoRef.current,
+      { rotate: 0 },
+      {
+        rotate: 720,
+        duration: 1.5,
+        ease: 'power2.out',
+      }
+    )
+  }, 5000)
+
+  return () => clearInterval(interval)
+}, [])
+
   return (
     <nav className='flexBetween max-container padding-container relative z-20 py-2'>
-        <Link
-        href="/">
+      <Link href='/'>
+        <div ref={logoRef}>
           <Image src='/hilink-logo.svg' alt='logo' width={80} height={29} />
-
-        </Link>
-        <ul className='hidden h-full gap-12 lg:flex'>
-            {NAV_LINKS.map((link)=>(
-              <Link
-              href={link.href} key={link.key} className="regular-16 text-grey-50 felxCenter cursor-pointer pb-1.5 transition-all hover:font-bold">  {link.label}
-              </Link>
-            ))}
-        </ul>
-        <div className='lg:flexCenter hidden'>
-            <Button
-            type='button'
-            title='Login'
-            icon='/user.svg'
-            variant='btn_dark_green'
-            />
         </div>
-        </nav>
+      </Link>
+
+      <ul className='hidden h-full gap-12 lg:flex'>
+        {NAV_LINKS.map((link) => (
+          <Link
+            href={link.href}
+            key={link.key}
+            className='regular-16 text-grey-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold'
+          >
+            {link.label}
+          </Link>
+        ))}
+      </ul>
+
+      <div className='lg:flexCenter hidden'>
+        <Button
+          type='button'
+          title='Login'
+          icon='/user.svg'
+          variant='btn_dark_green'
+        />
+      </div>
+    </nav>
   )
 }
 
